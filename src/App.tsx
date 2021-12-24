5b62e7a242d6dd8824fafce5b66ee36e6fae43f6
Tyler Glowaski <glowaski@Tylers-MacBook-Pro-5.local>
2021-12-24 13:07:14 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SayHello from 'tyler-ui-kit';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SayHello name="Tyler"/>
+    </div>
+  );
+}
+
+export default App;
